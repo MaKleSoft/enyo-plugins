814c64d76ede28f924ca72a968434258f7557789
Rafa Bernad <newness@newnessdevelopments.com>
2012-03-28 00:53:29 +0300

    Update v2/SelectDecorator/SelectDecorator.js

--- v2/SelectDecorator/SelectDecorator.js
+++ v2/SelectDecorator/SelectDecorator.js
@@ -1,37 +1,37 @@
 enyo.kind({
-	name : "SelectDecorator",
-	classes : "onyx-button select-decorator",
-	handlers : {
+	name: "SelectDecorator",
+	classes: "onyx-button select-decorator",
+	handlers: {
 		onchange : "changeHandler"
 	},
-	published : {
+	published: {
 		disabled : false,
 		showCaption : true,
 		showArrow : true,
-		icon : ""
+		icon: ""
 	},
 	showCaptionChanged: function() {
 		this.$.innerText.setShowing(this.showCaption);
 		this.addRemoveClass("select-decorator-no-caption", !this.showCaption);
 	},
-	disabledChanged : function() {
+	disabledChanged: function() {
 		this.addRemoveClass("select-decorator-disabled", this.disabled);
 	},
-	showArrowChanged : function() {
+	showArrowChanged: function() {
 		this.addRemoveClass("select-decorator-no-arrow", !this.showArrow);
 	},
-	iconChanged : function() {
+	iconChanged: function() {
 		this.$.innerIcon.setStyle("background-image: url('" + this.icon + "')");
 		this.$.innerIcon.setShowing((this.icon !== ""));
 	},
-	create : function() {
+	create: function() {
 		this.inherited(arguments);
 		this.disabledChanged();
 		this.showCaptionChanged();
 		this.showArrowChanged();
 		this.iconChanged();
 	},
-	rendered : function() {
+	rendered: function() {
 		this.inherited(arguments);
 		var select = this.getClientControls()[0];
 
@@ -39,23 +39,15 @@ enyo.kind({
 			this.changeHandler(select);
 		}
 	},
-	changeHandler : function(sender, event) {
-		//if(this.showCaption) {
+	changeHandler: function(sender, event) {
 		var caption = sender.getControls()[sender.getSelected()].getContent();
 		this.$.innerText.setContent(caption);
-		//}
 	},
-	components : [{
-		classes : "select-decorator-inner",
-		components : [ {
-			classes : "select-decorator-inner-arrow"
-		},{
-			name : "innerIcon",
-			classes : "select-decorator-inner-icon"
-		}, {
-			name : "innerText",
-			fit : true,
-			classes : "select-decorator-inner-text"
-		}]
-	}]
+	components: [
+		{classes: "select-decorator-inner", components: [
+			{classes: "select-decorator-inner-arrow"},
+			{name: "innerIcon", classes: "select-decorator-inner-icon"},
+			{name: "innerText",	fit : true,	classes : "select-decorator-inner-text"}
+		]}
+	]
 });
